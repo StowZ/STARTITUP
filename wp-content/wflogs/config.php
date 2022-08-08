ea18d4d8e7c1ec7c0ec9ddad3f944c829bb8dbff
Stow <91526733+naftiaziz@users.noreply.github.com>
2022-08-08 10:21:11 +0100

    Update
    Deploy

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1653487786;s:7:"authKey";s:64:"v703G`3sA2uIr EN(SKGuR(.AP5=<eEPSo[jPP_,v}?!0Jm,,gV5UN&q%Y!]7.YG";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1403;}
